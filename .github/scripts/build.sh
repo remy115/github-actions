620191c823ffb529b191737585ad80bb030c327e
Remy Ponso <remy.ponso@gmail.com>
2020-12-04 23:47:39 -0300

    testing if

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "################################3 OIOIoi ###########################33" $1
+
+if [ "admin" == $1 ]; then
+  echo "ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN ADMIN "
+fi
+
+exit 0
