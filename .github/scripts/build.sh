f594468f15e0e6c777c9525a17f961ef16e36439
Remy Ponso <remy.ponso@gmail.com>
2020-12-06 11:49:40 -0300

    testing sh script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,6 +4,7 @@ echo "################################3 OIOIoi ###########################33" $1
 
 if [ "api" == $1 ]; then
   echo "API API API API API API API API API API API API API API "
+  echo  pwd-- `pwd`
 fi
 
 exit 0
