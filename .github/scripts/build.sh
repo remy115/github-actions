94eb909daa7bcea2b9d14f4fa578dfe1bf63928f
Remy Ponso <remy.ponso@gmail.com>
2020-12-06 11:53:27 -0300

    testing sh script2

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,6 +5,11 @@ echo "################################3 OIOIoi ###########################33" $1
 if [ "api" == $1 ]; then
   echo "API API API API API API API API API API API API API API "
   echo  pwd-- `pwd`
+  echo "BEFORE COPY"
+  echo `ls -lha ./api`
+  rm -rf ./api/themes; cp -r ./theme/built ./api/
+  echo "AFTER COPY"
+  echo `ls -lha ./api`
 fi
 
 exit 0
